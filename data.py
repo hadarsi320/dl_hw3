d703a3cd6d85bf02f4c5ab4bce13aa33afa34edc
Hadar <hadarsi320@gmail.com>
2021-01-23 10:46:46 +0200

    Started loading data

--- data.py
+++ data.py
@@ -1,17 +1,27 @@
 import torch
 import torchvision.datasets as dsets
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor, Compose, Resize
 
-BATCH_SIZE = 128
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+BATCH_SIZE = 64
 
 
 def main():
+    transforms = Compose([ToTensor()])
     train_dataset = dsets.CelebA(root='/datashare/',
-                                 split='train',
+                                 split='train', transform=transforms,
                                  download=False)  # make sure you set it to False
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=BATCH_SIZE,
-                                               shuffle=True)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    for i, (x, y) in tqdm(enumerate(train_dataset)):
+        if i == 100:
+            break
+        x = Resize((64, 64))(x).unsqueeze(0)
 
 
 if __name__ == '__main__':
