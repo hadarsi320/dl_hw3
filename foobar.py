78323189137814c3c46e540f007dd1fae24439e3
Hadar <hadarsi320@gmail.com>
2021-01-23 21:58:51 +0200

    one module for messing around (foobar) and the other for loading and resizing the data

--- foobar.py
+++ foobar.py
@@ -9,19 +9,36 @@ from modules.joint_vae import JointVAE
 
 from tqdm import tqdm
 
+BATCH_SIZE = 64
+TEMP = 0.67
+EPOCHS = 100
+GAMMA = 100
+C_cont = (0, 50, 1e5)
+C_disc = (0, 10, 1e5)
+LEARNING_RATE = 5e-4
+
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    transforms = Compose([ToTensor(), Resize((64, 64))])
+    transforms = Compose([ToTensor()])
     train_dataset = dsets.CelebA(root='/datashare/',
                                  split='train', transform=transforms,
                                  download=False)  # make sure you set it to False
 
     # train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    resize = Resize((64, 64))
 
-    for x, _ in tqdm(train_dataset):
-        pass
+    # latent_space = {'cont': 32, 'disc': [10]}
+    latent_space = {'disc': [10]}
+    model = JointVAE(latent_space, TEMP, device)
+    model.to(device)
+
+    for i, (x, _) in tqdm(enumerate(train_dataset), total=100):
+        if i == 100:
+            break
+        x = resize(x).unsqueeze(0).to(device)
+        rec = model(x)
 
 
 if __name__ == '__main__':
