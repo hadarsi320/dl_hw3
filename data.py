236615c2a325883f83d6ca0fffaab45cc956889f
Hadar <hadarsi320@gmail.com>
2021-01-24 13:16:00 +0200

    Created a module for resizing and saving the images

--- data.py
+++ data.py
@@ -1,27 +1,26 @@
+import os
+
 import torch
 import torchvision.datasets as dsets
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor, Compose, Resize
 
-import matplotlib.pyplot as plt
-
-from modules.joint_vae import JointVAE
-
 from tqdm import tqdm
+
+BATCH_SIZE = 64
 
 
 def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+    data_dir = 'celeba_resized'
     transforms = Compose([ToTensor(), Resize((64, 64))])
     train_dataset = dsets.CelebA(root='/datashare/',
                                  split='train', transform=transforms,
                                  download=False)  # make sure you set it to False
 
-    # train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
-    for x, _ in tqdm(train_dataset):
-        pass
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+    for i, (x, _) in tqdm(enumerate(train_dataset), total=len(train_dataset), desc='Saving Images'):
+        torch.save(x, '{}/{}.pt'.format(data_dir, str(i).zfill(6)))
 
 
 if __name__ == '__main__':
