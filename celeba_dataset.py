1034e3782596392aa8d87bfcc762281929bb96c4
Hadar <hadarsi320@gmail.com>
2021-01-24 13:30:57 +0200

    Created dataset for celeba

---
+++ celeba_dataset.py
@@ -0,0 +1,23 @@
+import os
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class CelebADataset(Dataset):
+    def __init__(self, data_dir):
+        super(CelebADataset, self).__init__()
+        self.data_dir = data_dir
+        self.images = self.load_images()
+
+    def load_images(self):
+        images = []
+        for file in os.listdir(self.data_dir):
+            images.append(torch.load(f'{self.data_dir}/{file}'))
+        return images
+
+    def __getitem__(self, item):
+        return self.images[item]
+
+    def __len__(self):
+        return len(self.images)
